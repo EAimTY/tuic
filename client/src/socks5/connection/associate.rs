d53840669972075a72bdeed74193cdbbb2b65207
EAimTY <ea.imty@gmail.com>
2022-03-12 03:35:51 +0900

    client: complete socks5 server UDP associate

--- client/src/socks5/connection/associate.rs
+++ client/src/socks5/connection/associate.rs
@@ -1,10 +1,16 @@
 use super::Connection;
-use crate::client::{Address as RelayAddress, Request as RelayRequest};
-use crate::socks5::protocol::{Address, Reply, Response, UdpHeader};
+use crate::{
+    client::{Address as RelayAddress, Request as RelayRequest},
+    socks5::protocol::{Address, Reply, Response, UdpHeader},
+};
 use anyhow::{bail, Result};
 use bytes::Bytes;
-use std::net::SocketAddr;
-use tokio::{net::UdpSocket, sync::mpsc::Sender as MpscSender};
+use std::{net::SocketAddr, sync::Arc};
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpStream, UdpSocket},
+    sync::mpsc::{Receiver as MpscReceiver, Sender as MpscSender},
+};
 
 impl Connection {
     pub async fn handle_associate(mut self, addr: Address) -> Result<()> {
@@ -25,7 +31,7 @@ impl Connection {
             }
         };
 
-        let socket = UdpSocket::bind(SocketAddr::from(([0, 0, 0, 0], 0))).await?;
+        let socket = Arc::new(UdpSocket::bind(SocketAddr::from(([0, 0, 0, 0], 0))).await?);
         let socket_addr = socket.local_addr()?;
 
         let resp = Response::new(Reply::Succeeded, Address::SocketAddress(socket_addr));
@@ -47,20 +53,64 @@ impl Connection {
             }
         }
 
-        socket.connect(src_addr.unwrap()).await?;
+        let src_addr = unsafe { src_addr.unwrap_unchecked() };
+        socket.connect(src_addr).await?;
 
-        loop {
-            let mut buf = vec![0; 1536];
-            let len = socket.recv(&mut buf).await?;
-            buf.truncate(len);
-
-            match send_packet(buf, &pkt_send_tx).await {
-                Ok(()) => (),
-                Err(err) => eprintln!("{err}"),
-            }
-        }
+        tokio::try_join!(
+            listen_send(socket.clone(), pkt_send_tx),
+            listen_receive(socket, pkt_receive_rx),
+            listen_termination(self.stream)
+        )?;
 
         Ok(())
+    }
+}
+
+async fn listen_send(
+    socket: Arc<UdpSocket>,
+    pkt_send_tx: MpscSender<(Bytes, RelayAddress)>,
+) -> Result<()> {
+    loop {
+        let mut buf = vec![0; 1536];
+        let len = socket.recv(&mut buf).await?;
+        buf.truncate(len);
+
+        match send_packet(buf, &pkt_send_tx).await {
+            Ok(()) => (),
+            Err(err) => eprintln!("{err}"),
+        }
+    }
+}
+
+async fn listen_receive(
+    socket: Arc<UdpSocket>,
+    mut pkt_receive_rx: MpscReceiver<(Bytes, RelayAddress)>,
+) -> Result<()> {
+    while let Some((packet, addr)) = pkt_receive_rx.recv().await {
+        let addr = match addr {
+            RelayAddress::SocketAddress(addr) => Address::SocketAddress(addr),
+            RelayAddress::HostnameAddress(hostname, port) => {
+                Address::HostnameAddress(hostname, port)
+            }
+        };
+
+        let udp_header = UdpHeader::new(0, addr);
+
+        let mut buf = vec![0; udp_header.serialized_len() + packet.len()];
+        udp_header.write_to_buf(&mut buf);
+        buf[udp_header.serialized_len()..].copy_from_slice(&packet);
+
+        socket.send(&buf).await?;
+    }
+
+    bail!("UDP packet channel closed")
+}
+
+async fn listen_termination(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0; 8];
+
+    loop {
+        stream.read_exact(&mut buf).await?;
     }
 }
 
@@ -71,14 +121,14 @@ async fn send_packet(buf: Vec<u8>, pkt_send_tx: &MpscSender<(Bytes, RelayAddress
         bail!("Fragmented UDP packet is not supported");
     }
 
-    let bytes = Bytes::from(buf).slice(udp_header.serialized_len()..);
+    let packet = Bytes::from(buf).slice(udp_header.serialized_len()..);
 
     let dst_addr = match udp_header.address {
         Address::SocketAddress(addr) => RelayAddress::SocketAddress(addr),
         Address::HostnameAddress(hostname, port) => RelayAddress::HostnameAddress(hostname, port),
     };
 
-    let _ = pkt_send_tx.send((bytes, dst_addr)).await;
+    let _ = pkt_send_tx.send((packet, dst_addr)).await;
 
     Ok(())
 }
