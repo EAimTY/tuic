5257452c22b88c4e7c167c7b88e5033477c83ef0
EAimTY <ea.imty@gmail.com>
2022-03-20 02:01:21 +0900

    server: remove `der-parser` dependency

--- server/src/certificate.rs
+++ server/src/certificate.rs
@@ -7,54 +7,43 @@ use std::{
 use thiserror::Error;
 
 pub fn load_certificates(path: &str) -> Result<Vec<Certificate>, CertificateError> {
-    let file = fs::read(path)?;
-
-    if der_parser::parse_der(&file).is_ok() {
-        return Ok(vec![Certificate(file)]);
-    }
-
     let mut file = BufReader::new(File::open(path)?);
     let mut certs = Vec::new();
 
-    while let Some(item) = rustls_pemfile::read_one(&mut file)? {
+    while let Ok(Some(item)) = rustls_pemfile::read_one(&mut file) {
         if let Item::X509Certificate(cert) = item {
             certs.push(Certificate(cert));
         }
     }
 
-    if !certs.is_empty() {
-        Ok(certs)
-    } else {
-        Err(CertificateError::NoCertificateFound)
+    if certs.is_empty() {
+        certs = vec![Certificate(fs::read(path)?)];
     }
+
+    Ok(certs)
 }
 
 pub fn load_private_key(path: &str) -> Result<PrivateKey, CertificateError> {
-    let file = fs::read(path)?;
+    let mut file = BufReader::new(File::open(path)?);
+    let mut priv_key = None;
 
-    if der_parser::parse_der(&file).is_ok() {
-        return Ok(PrivateKey(file));
+    while let Ok(Some(item)) = rustls_pemfile::read_one(&mut file) {
+        if let Item::RSAKey(key) | Item::PKCS8Key(key) = item {
+            priv_key = Some(PrivateKey(key));
+        }
     }
 
-    let mut file = BufReader::new(File::open(path)?);
+    if priv_key.is_none() {
+        priv_key = Some(PrivateKey(fs::read(path)?));
+    }
 
-    let key = loop {
-        match rustls_pemfile::read_one(&mut file)? {
-            Some(Item::RSAKey(key)) | Some(Item::PKCS8Key(key)) => break PrivateKey(key),
-            None => return Err(CertificateError::NoPrivateKeyFound),
-            _ => {}
-        }
-    };
+    let priv_key = unsafe { priv_key.unwrap_unchecked() };
 
-    Ok(key)
+    Ok(priv_key)
 }
 
 #[derive(Error, Debug)]
 pub enum CertificateError {
     #[error(transparent)]
     Io(#[from] IoError),
-    #[error("No valid certificate found in file")]
-    NoCertificateFound,
-    #[error("No valid private key found in file")]
-    NoPrivateKeyFound,
 }
