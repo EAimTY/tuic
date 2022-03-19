36f632213bf26c6e87dc97ff6ee48e9472af80a6
EAimTY <ea.imty@gmail.com>
2022-03-20 02:01:14 +0900

    client: remove `der-parser` dependency

--- client/src/certificate.rs
+++ client/src/certificate.rs
@@ -7,32 +7,24 @@ use std::{
 use thiserror::Error;
 
 pub fn load_certificates(path: &str) -> Result<Vec<Certificate>, CertificateError> {
-    let file = fs::read(path)?;
-
-    if der_parser::parse_der(&file).is_ok() {
-        return Ok(vec![Certificate(file)]);
-    }
-
     let mut file = BufReader::new(File::open(path)?);
     let mut certs = Vec::new();
 
-    while let Some(item) = rustls_pemfile::read_one(&mut file)? {
+    while let Ok(Some(item)) = rustls_pemfile::read_one(&mut file) {
         if let Item::X509Certificate(cert) = item {
             certs.push(Certificate(cert));
         }
     }
 
-    if !certs.is_empty() {
-        Ok(certs)
-    } else {
-        Err(CertificateError::NoCertificateFound)
+    if certs.is_empty() {
+        certs = vec![Certificate(fs::read(path)?)];
     }
+
+    Ok(certs)
 }
 
 #[derive(Error, Debug)]
 pub enum CertificateError {
     #[error(transparent)]
     Io(#[from] IoError),
-    #[error("No valid certificate found in file")]
-    NoCertificateFound,
 }
