f6ecd5edf2245caed3f25317b25e829cea97e79a
EAimTY <ea.imty@gmail.com>
2022-08-02 02:49:49 +0900

    optimizing `SplitPacket`

--- src/common/udp.rs
+++ src/common/udp.rs
@@ -15,8 +15,8 @@ pub fn split_packet(pkt: Bytes, addr: &Address, max_datagram_len: usize) -> Spli
 pub struct SplitPacket {
     pkt: Bytes,
     max_pkt_size: usize,
-    start: usize,
-    end: usize,
+    next_start: usize,
+    next_end: usize,
     len: usize,
 }
 
@@ -44,8 +44,8 @@ impl SplitPacket {
         Self {
             pkt,
             max_pkt_size,
-            start: 0,
-            end: first_pkt_size,
+            next_start: 0,
+            next_end: first_pkt_size,
             len,
         }
     }
@@ -55,10 +55,14 @@ impl Iterator for SplitPacket {
     type Item = Bytes;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.start <= self.pkt.len() {
-            let next = self.pkt.slice(self.start..self.end.min(self.pkt.len()));
-            self.start += self.max_pkt_size;
-            self.end += self.max_pkt_size;
+        if self.next_start <= self.pkt.len() {
+            let next = self
+                .pkt
+                .slice(self.next_start..self.next_end.min(self.pkt.len()));
+
+            self.next_start += self.max_pkt_size;
+            self.next_end += self.max_pkt_size;
+
             Some(next)
         } else {
             None
@@ -67,6 +71,7 @@ impl Iterator for SplitPacket {
 }
 
 impl ExactSizeIterator for SplitPacket {
+    #[inline]
     fn len(&self) -> usize {
         self.len
     }
